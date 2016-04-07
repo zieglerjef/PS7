475945639b43620cb157898f07061a7bc05b0c9e
zieglerjef <zieglerjef@gmail.com>
2016-04-06 22:04:43 -0500

    Allow for multiple dimensions

--- PS7.R
+++ PS7.R
@@ -17,31 +17,57 @@ sg.int <- function(g,..., lower, upper, dimensions, parallelCores=1){
   # allows the function to have parallel processing
   # doParellel allows for multicore functionality even w/ Windows
   registerDoParallel(cores=parallelCores)
-  
-  lower<-floor(lower)
-  
-  upper<-ceiling(upper)
-  
-  if (any(lower>upper)) stop("lower must be smaller than upper")
-  
-  gridss<-as.matrix(expand.grid(seq(lower[1],upper[1]-1,by=1),seq(lower[2],upper[2]-1,by=1)))
-  
-  sp.grid <- createIntegrationGrid( 'KPU', dimension=2, k=5 )
-  
-  nodes<-gridss[1,]+sp.grid$nodes
-  
-  weights<-sp.grid$weights
-  
-  for (i in 2:nrow(gridss))
-    
-  {
-    nodes<-rbind(nodes,gridss[i,]+sp.grid$nodes)  
-    
-    weights<-c(weights,sp.grid$weights)
-    
-  }
-  
+  # round down at lower threshold
+  lower <- floor(lower)
+  # round up at uppper threshold
+  upper <- ceiling(upper)
+  # throw error if lower is larger than upper
+  if (any(lower > upper)) stop("Lower must be smaller than upper.")
+  # apply the function to all the dimensions specified by user, create vector each iteration
+  # create data frame from all combination of vectors and turn into matrix
+  gridss <- as.matrix(expand.grid(unlist(lapply(1:dimensions, function(i) seq(lower[i], upper[i]-1, by=1)))))
+  # create nodes an weights to be used for integration from SpareGrid package
+  # allow for multiple dimensions set by user
+  sp.grid <- createIntegrationGrid('KPU', dimension=dimensions, k=5)
+  # assign matrix w/ nodes by row from grid object
+  nodes <- gridss[1,] + sp.grid$nodes
+  # assign weights from grid object
+  weights <- sp.grid$weights
+
+    for (i in 2:nrow(gridss)) {
+      # combine nodes by row
+      nodes <- rbind(nodes, gridss[i,] + sp.grid$nodes)  
+      # concatenate weights
+      weights <- c(weights, sp.grid$weights)
+    }
+  # apply function to nodes matrix
   gx.sp <- apply(nodes, 1, g,...)
-  val.sp <- gx.sp %*%weights
+  # matrix multiply nodes by weights
+  val.sp <- gx.sp %*% weights
+  # return product
   val.sp
 }
+
+####################
+### Unit testing ###
+####################
+
+#################
+### Benchmark ###
+#################
+
+## measure speed depending on 1) number of dimensions and
+## 2) number of cores
+
+# create test function
+testFn0 <- function(x) {
+  prod(cos(x))
+}
+
+# run sg.int function with three-dimensions, comparing 2 versus 4
+microbenchmark(sg.int(testFn0, lower=rep(0,3), upper=rep(1,3), dimensions=3, parallelCores=2), times=100)
+microbenchmark(sg.int(testFn0, lower=rep(0,3), upper=rep(1,3), dimensions=3, parallelCores=4), times=100)
+
+# run sg.int function with five-dimensions, comparing 2 versus 4
+microbenchmark(sg.int(testFn0, lower=rep(0,5), upper=rep(1,5), dimensions=5, parallelCores=2), times=100)
+microbenchmark(sg.int(testFn0, lower=rep(0,5), upper=rep(1,5), dimensions=5, parallelCores=4), times=100)
