32901fb879a3c91efd5803402a10dab42634b404
zieglerjef <zieglerjef@gmail.com>
2016-04-07 18:36:56 -0500

    add comparison (speed and accuracy) section

--- PS7.R
+++ PS7.R
@@ -54,3 +54,26 @@ testFn2 <- function(x) {
 # run sg.int function with five-dimensions, w/ and w/o parallelCores
 microbenchmark(sg.int(testFn2, lower=rep(0,5), upper=rep(1,5), parallelCores=TRUE), times=100)
 microbenchmark(sg.int(testFn2, lower=rep(0,5), upper=rep(1,5), parallelCores=FALSE), times=100)
+
+####################################
+### Comparison w/ adaptIntegrate ###
+####################################
+
+### accuracy ###
+adaptIntegrate(testFn1, rep(0,3), rep(1,3))
+sg.int(testFn1, lower=rep(0,3), upper=rep(1,3))
+
+adaptIntegrate(testFn2, rep(0,5), rep(1,5))
+sg.int(testFn2, lower=rep(0,5), upper=rep(1,5))
+
+## get very similar answers, but adaptIntegrate slightly more accurate
+
+### speed ###
+
+microbenchmark(adaptIntegrate(testFn1, rep(0,3), rep(1,3)), times=100)
+microbenchmark(sg.int(testFn1, lower=rep(0,3), upper=rep(1,3), parallelCores=FALSE), times=100)
+
+microbenchmark(adaptIntegrate(testFn2, rep(0,5), rep(1,5)), times=100)
+microbenchmark(sg.int(testFn2, lower=rep(0,5), upper=rep(1,5), parallelCores=FALSE), times=100)
+
+## sg.int is faster, but this is largely a function of the tolerance and maxEval defaults
