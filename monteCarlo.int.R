e441a2fc57878f3a300d45c9ce87d2a3c523296f
zieglerjef <zieglerjef@gmail.com>
2016-04-07 20:32:34 -0500

    add parallel functionality

--- monteCarlo.int.R
+++ monteCarlo.int.R
@@ -10,7 +10,17 @@ monteCarlo.int <- function(g,..., lower, upper, n, dimensions, parallelCores=TRU
   u <- runif(n*dimensions, lower, upper)
   # place in matrix w/ each column = dimension
   these <- matrix(u, ncol=dimensions)
-  
-  x <- apply(these, 1, g,...)
+  # allow parallel processing
+  if(parallelCores==T){
+    # detect the number of cores
+    nCores <- detectCores() - 1
+    # initiate cluster
+    cl <- makeCluster(nCores)
+    x <- parRapply(cl, x=these, FUN=g,...)
+    stopCluster(cl)
+  }
+  else{
+    x <- apply(these, 1, g,...)
+  }
   return(mean(x)*(upper-lower)^dimensions)
 }
